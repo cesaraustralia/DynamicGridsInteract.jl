855440a19c7bcb3b0bce042b9fe09e300b69ee40
Rafael Schouten <rafaelschouten@gmail.com>
2020-11-05 04:25:24 +1100

    use MP for interface

--- src/interact.jl
+++ src/interact.jl
@@ -1,13 +1,4 @@
 
-# const csskey = AssetRegistry.register(joinpath(dirname(pathof(DynamicGridsInteract)), "../assets/web.css"))
-
-# TODO update themes
-# Custom css theme
-# struct WebTheme <: WidgetTheme end
-
-# libraries(::WebTheme) = vcat(libraries(InteractBulma.BulmaTheme()), [csskey])
-
-
 """
 Abstract supertype of Interact outputs including `InteractOuput` and `ElectronOutput`
 """
@@ -46,55 +37,94 @@ mutable struct InteractOutput{T,F<:AbstractVector{T},E,GC,IC,RS<:Ruleset,Pa,IM,T
     image_obs::IM
     t_obs::TI
 end
-# Defaults are passed in from ImageOutput constructor
-InteractOutput(; frames, running, extent, graphicconfig, imageconfig,
-               ruleset, extrainit=Dict(), throttle=0.1, interactive=true, kwargs...) = begin
-
-    # settheme!(theme)
-
-    # Standard output and controls
+# Most defaults are passed in from the generic ImageOutput constructor
+function InteractOutput(; 
+    frames, running, extent, graphicconfig, imageconfig,
+    ruleset, extrainit=Dict(), throttle=0.1, interactive=true, kwargs...
+)
+    # Observables that update during the simulation
     image_obs = Observable{Any}(dom"div"())
-
-    timedisplay = Observable{Any}(dom"div"("0"))
     t_obs = Observable{Int}(1)
-    map!(timedisplay, t_obs) do t
-        dom"div"(string(t))
-    end
+
+    # Page and output construction
     page = vbox()
-    o = InteractOutput(
+    output = InteractOutput(
          frames, running, extent, graphicconfig, imageconfig, ruleset, page, image_obs, t_obs
     )
 
-    # timespan_obs = Observable{Int}(DynamicGrids.stoptime(ui))
-    # timespan_text = textbox("1000")
-    # map!(timespan_obs, observe(timespan_text)) do ts
-        # parse(Int, ts)
-    # end
+    # Widgets
+    timedisplay = time_text(t_obs)
+    controls = control_widgets(output, ruleset)
+    sliders = rule_sliders(ruleset, throttle, groups, interactive)
 
+    # Put it all together into a web page
+    output.page = vbox(hbox(output.image_obs), timedisplay, controls, sliders)
+
+    # Initialise image Observable
+    simdata = DynamicGrids.SimData(extent, ruleset)
+    image_obs[] = _webimage(DG.grid2image(o, simdata, o[1], 1, first(extent.tspan)))
+
+    return hbox(sim, resume, stop, fps_slider, init_drop...)
+end
+
+# Base interface
+Base.display(o::InteractOutput) = display(o.page)
+Base.show(o::InteractOutput) = show(o.page)
+
+# DynamicGrids interface
+DynamicGrids.isasync(o::InteractOutput) = true
+function DynamicGrids.showimage(image::AbstractArray, o::InteractOutput, f, t)
+    println("frame: $f at: $t")
+    # Update simulation image, makeing sure any errors are printed in the REPL
+    try
+        o.t_obs[] = f
+        o.image_obs[] = _webimage(image)
+    catch e
+        println(e)
+    end
+    return nothing
+end
+
+_webimage(image) = dom"div"(image)
+
+
+
+# Widget buliding
+
+function time_text(t_obs::Observable)
+    timedisplay = Observable{Any}(dom"div"("0"))
+    map!(timedisplay, t_obs) do t
+        dom"div"(string(t))
+    end
+    return timedisplay
+end
+
+function rule_sliders(ruleset, throttle, groups, interactive)
+    if interactive 
+        return ModelParameters.attach_sliders!(ruleset; throttle=throttle, grouped=grouped) 
+    else
+        return dom"div"()
+    end
+end
+
+function control_widgets(o::InteractOutput, ruleset)
+    # We use the init dropdown for the simulation init, even if we don't 
+    # show the dropdown because it only has 1 option.
     extrainit[:init] = deepcopy(init(extent))
-    init_drop = dropdown(extrainit, value=extrainit[:init], label="Init")
+    init_dropdown = dropdown(extrainit, value=extrainit[:init], label="Init")
+    maybe_init_dropdown = length(extrainit) > 1 ? (init_dropdown,) : ()
 
+    fps_slider = slider(1:200, value=fps(graphicconfig), label="FPS")
+
+    # Buttons
     sim = button("sim")
     resume = button("resume")
     stop = button("stop")
 
-    buttons = sim, resume, stop
-    fps_slider = slider(1:200, value=fps(graphicconfig), label="FPS")
-    basewidgets = hbox(buttons..., fps_slider, (length(extrainit) > 1 ? (init_drop,) : ())...)
-
-    rulesliders = interactive ? buildsliders(ruleset, throttle) : dom"div"()
-
-    # Put it all together into a webpage
-    o.page = vbox(hbox(o.image_obs), timedisplay, basewidgets, rulesliders)
-
-    # Initialise image
-    simdata = DynamicGrids.SimData(extent, ruleset)
-    image_obs[] = webimage(DG.grid2image(o, simdata, o[1], 1, first(extent.tspan)))
-
     # Control mappings. Make errors visible in the console.
     on(observe(sim)) do _
         try
-            !isrunning(o) && sim!(o, ruleset; init=init_drop[])
+            !isrunning(o) && sim!(o, ruleset; init=init_dropdown[])
         catch e
             println(e)
         end
@@ -122,72 +152,5 @@ InteractOutput(; frames, running, extent, graphicconfig, imageconfig,
         end
     end
 
-    return o
+    return hbox(sim, resume, stop, fps_slider, maybe_init_dropdown...)
 end
-
-# Base interface
-Base.display(o::InteractOutput) = display(o.page)
-Base.show(o::InteractOutput) = show(o.page)
-
-# DynamicGrids interface
-DynamicGrids.isasync(o::InteractOutput) = true
-
-DynamicGrids.showimage(image::AbstractArray, o::InteractOutput, f, t) = begin
-    println("frame: $f at: $t")
-    try
-        o.t_obs[] = f
-        o.image_obs[] = webimage(image)
-    catch e
-        println(e)
-    end
-end
-
-
-# Utils
-
-buildsliders(ruleset, _throttle) = begin
-    rs = rules(ruleset)
-    params = Flatten.flatten(rs)
-    fnames = fieldnameflatten(rs)
-    bounds_ = metaflatten(rs, FieldMetadata.bounds)
-    ranges = buildrange.(bounds_, params)
-    parents = Tuple(string(p)[1] == '#' ? "" : p for p in parentnameflatten(rs))
-    descriptions = metaflatten(rs, FieldMetadata.description)
-    attributes = (p, n, d) -> Dict(:title => "$p.$n: $d").(parents, fnames, descriptions)
-
-
-    sliders = buildslider.(params, fnames, ranges, attributes)
-    slider_obs = map((s...) -> s, throttle.(_throttle, observe.(sliders))...)
-    on(slider_obs) do s
-        try
-            ruleset.rules = Flatten.reconstruct(ruleset.rules, s)
-        catch e
-            println(e)
-        end
-    end
-
-    group_title = nothing
-    slider_groups = []
-    group_items = []
-    for i in 1:length(params)
-        parent = parents[i]
-        if group_title != parent
-            group_title == nothing || push!(slider_groups, dom"div"(group_items...))
-            group_items = Any[dom"h2"(string(parent))]
-            group_title = parent
-        end
-        push!(group_items, sliders[i])
-    end
-    push!(slider_groups, dom"h2"(group_items...))
-
-    vbox(slider_groups...)
-end
-
-
-buildslider(val, lab, rng, attr) = slider(rng; label=string(lab), value=val, attributes=attr)
-
-buildrange(lim::Tuple{AbstractFloat,AbstractFloat}, val::T) where T =
-    T(lim[1]):(T(lim[2])-T(lim[1]))/1000:T(lim[2])
-buildrange(lim::Tuple{Int,Int}, val::T) where T = T(lim[1]):1:T(lim[2])
-
-webimage(image) = dom"div"(image)
