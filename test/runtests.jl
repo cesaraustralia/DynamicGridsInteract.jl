5d1bc3fb323334bb1b29689cabd31a3381ff138e
Rafael Schouten <rafaelschouten@gmail.com>
2020-12-16 16:49:48 +1100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,16 +23,9 @@ test5 = [0 0 0 0 0 0
          0 0 0 0 0 1
          0 0 0 0 0 0]
 
-g0 = ARGB32(0)
-g1 = ARGB32(1)
-grey2 = [g0 g0 g0 g0 g0 g0
-         g0 g0 g0 g0 g0 g0
-         g1 g0 g0 g0 g1 g1
-         g1 g0 g0 g0 g0 g0
-         g0 g0 g0 g0 g0 g1
-         g0 g0 g0 g0 g0 g0]
-
-processor = ColorProcessor(scheme=ColorSchemes.leonardo)
+processor = ColorProcessor(
+    scheme=ColorSchemes.leonardo, zerocolor=nothing, maskcolor=nothing, textconfig=nothing,
+)
 
 @testset "InteractOutput" begin
     l0 = ARGB32(get(ColorSchemes.leonardo, 0))
@@ -45,7 +38,7 @@ processor = ColorProcessor(scheme=ColorSchemes.leonardo)
                  l0 l0 l0 l0 l0 l1
                  l0 l0 l0 l0 l0 l0]
 
-    ruleset = Ruleset(Life(); overflow=WrapOverflow())
+    ruleset = Ruleset(Life(); boundary=Wrap())
     output = InteractOutput(init; tspan=1:2, ruleset=ruleset, store=true, processor=processor);
     sim!(output, ruleset)
     sleep(5)
@@ -75,7 +68,7 @@ end
 
 if !Sys.islinux() # No graphic head loaded in CI: TODO add this
     @testset "ElectronOutput" begin
-        ruleset = Ruleset(Life(); overflow=WrapOverflow())
+        ruleset = Ruleset(Life(); boundary=Wrap())
         output = ElectronOutput(init; ruleset=ruleset, tspan=1:3, store=true, processor=processor)
         DynamicGrids.setrunning!(output, false)
         sim!(output.interface, ruleset)
@@ -90,7 +83,7 @@ if !Sys.islinux() # No graphic head loaded in CI: TODO add this
 end
 
 @testset "ServerOutput" begin
-    ruleset = Ruleset(Life(); overflow=WrapOverflow())
+    ruleset = Ruleset(Life(); boundary=Wrap())
     ServerOutput(init; ruleset=ruleset, port=8080, processor=processor)
     # TODO: test the server somehow
 end
